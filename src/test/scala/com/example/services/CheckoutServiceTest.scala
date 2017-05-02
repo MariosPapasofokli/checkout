6da7d0ccc791354504fd03e636076d544618c355
Marios Papasofokli <marios.papasofokli@gmail.com>
2017-05-02 13:07:52 +0300

    CheckoutServiceTest refactor

--- src/test/scala/com/example/services/CheckoutServiceTest.scala
+++ src/test/scala/com/example/services/CheckoutServiceTest.scala
@@ -6,7 +6,7 @@ import org.specs2.mutable.Specification
 
 class CheckoutServiceTest extends Specification {
 
-  val input0 = Seq.empty
+  val input0 = Nil
   val input1 = List("apple", "APPLE", "orange", "orAnge", "orange")
   val input2 = List("pineapple", "berry")
 
@@ -15,7 +15,7 @@ class CheckoutServiceTest extends Specification {
   "validateInput method should" >> {
 
     "handle empty seq" >> {
-      checkoutService.validateInput(input0) must_== Seq.empty
+      checkoutService.validateInput(input0) must_== Nil
     }
 
     "validate a mixed seq of input strings with uppercase and lower case characters in strings" >> {
